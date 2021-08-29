4c84489e3baf16ad45614ea3062f2323a37a4453
Parag Paralikar <parag.paralikar@gmail.com>
2021-08-29 15:10:55 +0530

    na

--- sphinx-parent/sphinx-web/src/main/resources/ui/angular/src/app/workflows/workflow-editor/workflow-editor.component.ts
+++ sphinx-parent/sphinx-web/src/main/resources/ui/angular/src/app/workflows/workflow-editor/workflow-editor.component.ts
@@ -10,10 +10,10 @@ import { DiagramModel, NodeModel, PortModel, RxZuDiagramComponent } from '@rxzu/
 export class WorkflowEditorComponent {
   diagramModel: DiagramModel;
   nodesLibrary = [
-    { type:'request', name: 'Request Received', icon:'fa fa-arrow-right' },
-    { type:'email', name: 'Email', icon: 'fa fa-envelope'},
-    { type:'approval', name: 'Approval', icon: 'fa fa-check' },
-    { type:'ldap', name: 'LDAP', icon: 'fa fa-users' }
+    { type:'request', name: 'Request Received', icon:'fa fa-arrow-right', inputs: 0, outputs: 1, height: 100, width: 150 },
+    { type:'email', name: 'Email', icon: 'fa fa-envelope', inputs: 1, outputs: 1, height: 100, width: 150 },
+    { type:'approval', name: 'Approval', icon: 'fa fa-check', inputs: 1, outputs: 2, height: 100, width: 150 },
+    { type:'ldap', name: 'LDAP', icon: 'fa fa-users', inputs: 1, outputs: 1, height: 100, width: 150 }
   ];
   @ViewChild(RxZuDiagramComponent, { static: true })
   diagram?: RxZuDiagramComponent;
@@ -26,13 +26,34 @@ export class WorkflowEditorComponent {
     this.diagram?.zoomToFit();
   }
 
-  createNode(type: string) {
+  createNode(nodeDef) {
     const nodeData = {
-      type: type
+      type: nodeDef.type,
+      data: {},
+      status: undefined
     };
     const node = new NodeModel();
     node.setExtras(nodeData);
+    node.setHeight(nodeDef.height);
+    node.setWidth(nodeDef.width);
     return node;
+  }
+
+  createPorts(nodeDef: any, node: NodeModel){
+    if(0 < nodeDef.inputs){
+      const step = nodeDef.height / (nodeDef.inputs + 1);
+      for(var index = 1; index <= nodeDef!.inputs; index++){
+        const y = node.getCoords().x +  index * step;
+        const port = new PortModel();
+        port.updateCoords({
+          x: 0,
+          y: y,
+          width: 5,
+          height: 5
+        });
+        node.addPort(port);
+      }
+    }
   }
 
   onBlockDrag(e: DragEvent) {
@@ -45,7 +66,8 @@ export class WorkflowEditorComponent {
   onBlockDropped(e: DragEvent): void | undefined {
     if (e.dataTransfer) {
       const nodeType = e.dataTransfer.getData('type');
-      const node = this.createNode(nodeType);
+      const nodeDef = this.nodesLibrary.find(n => n.type == nodeType);
+      const node = this.createNode(nodeDef);
       const canvasManager = this.diagram?.diagramEngine.getCanvasManager();
       if (canvasManager) {
         const droppedPoint = canvasManager.getZoomAwareRelativePoint(e);
@@ -57,6 +79,7 @@ export class WorkflowEditorComponent {
         };
         if (node) {
           node.setCoords(coords);
+          this.createPorts(nodeDef, node);
           this.diagramModel.addNode(node);
         }
       }
